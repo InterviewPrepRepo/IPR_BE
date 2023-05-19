745d28ece2bbdd93018fb183aec4f17f7ae5b4ea
Minseon Song <songminseonk@gmail.com>
2023-05-19 14:57:20 -0400

    api key now stored in appsettings

--- Controllers/IMochaController.cs
+++ Controllers/IMochaController.cs
@@ -6,10 +6,14 @@ namespace IPR_BE.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class IMochaController : ControllerBase {
+    private readonly IConfiguration configuration;
+    public IMochaController(IConfiguration iConfig) {
+        configuration = iConfig;
+    }
     [HttpGet]
     public async Task GetAllTests() {
         HttpClient http = new HttpClient();
-        http.DefaultRequestHeaders.Add("X-API-KEY", "");
+        http.DefaultRequestHeaders.Add("X-API-KEY", configuration.GetValue<string>("IMocha:ApiKey"));
         string str = await http.GetStringAsync("https://apiv3.imocha.io/v3/tests");
         Console.WriteLine(str);
     }
