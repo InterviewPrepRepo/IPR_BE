779773bd413008e19ded2f0d09e7a00fd69b2b27
jdelacruz96 <jonathan.delacruz@revature.com>
2023-05-22 13:22:07 -0400

    cleanup take 2

--- Controllers/IMochaController.cs
+++ Controllers/IMochaController.cs
@@ -63,7 +63,6 @@ public class IMochaController : ControllerBase {
     /// <returns></returns>
     [HttpGet("reports/{testInvitationId}/questions")]
     public async Task<TestResultDTO> GetVidTestAttempt(int testInvitationId){
-        //JsonContent content = new JsonContent.Create<>();
         HttpResponseMessage response = new HttpResponseMessage();
         response = await http.PostAsync($"reports/{testInvitationId}/questions", null);
         string str = await response.Content.ReadAsStringAsync();
