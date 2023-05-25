02c6325a3e98cbcc6a8b1242c41bc776f9b39dcc
Minseon Song <songminseonk@gmail.com>
2023-05-25 10:20:27 -0400

    started interview bot repo to read from that db

---
+++ DataAccess/InterviewBotRepo.cs
@@ -0,0 +1,10 @@
+using System.Data.SqlClient;
+
+namespace IPR_BE.DataAccess;
+
+public class InterviewBotRepo {
+    private SqlConnection conn;
+    public InterviewBotRepo(string connStr) {
+        
+    }
+}
