d8697afced6fc4f9029a4ec6ceec44f6c9b09ae9
Minseon Song <songminseonk@gmail.com>
2023-07-12 16:56:02 -0400

    added missing skill file

---
+++ Migrations/20230712183134_additioanl-info-for-candidates.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IPR_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class additioanlinfoforcandidates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "currentRole",
+                table: "Candidates",
+                type: "varchar(255)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "yearsExperience",
+                table: "Candidates",
+                type: "integer",
+                nullable: true,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CandidateSkill",
+                columns: table => new
+                {
+                    CandidateIdid = table.Column<int>(type: "int", nullable: false),
+                    SkillIdid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CandidateSkill", x => new { x.CandidateIdid, x.SkillIdid });
+                    table.ForeignKey(
+                        name: "FK_CandidateSkill_Candidates_CandidateIdid",
+                        column: x => x.CandidateIdid,
+                        principalTable: "Candidates",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CandidateSkill_Skills_SkillIdid",
+                        column: x => x.SkillIdid,
+                        principalTable: "Skills",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CandidateSkill_SkillIdid",
+                table: "CandidateSkill",
+                column: "SkillIdid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CandidateSkill");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropColumn(
+                name: "currentRole",
+                table: "Candidates");
+
+            migrationBuilder.DropColumn(
+                name: "yearsExperience",
+                table: "Candidates");
+        }
+    }
+}
