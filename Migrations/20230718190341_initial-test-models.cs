338bcd8f62d7a691c5bec770977759625fccddf6
jdelacruz96 <jonathan.delacruz@revature.com>
2023-07-19 11:15:01 -0400

    added new migrations

---
+++ Migrations/20230718190341_initial-test-models.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IPR_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialtestmodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "comment",
+                table: "TestAttempts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "createdOn",
+                table: "TestAttempts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "endDate",
+                table: "TestAttempts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "startDate",
+                table: "TestAttempts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "totalScore",
+                table: "TestAttempts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    questionId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    prompt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    questionTypeId = table.Column<int>(type: "int", nullable: false),
+                    possiblePoints = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.questionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionTypes",
+                columns: table => new
+                {
+                    questionTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionTypes", x => x.questionTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    tagId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.tagId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestAttemptQuestionSections",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    testAttemptSectionId = table.Column<long>(type: "bigint", nullable: false),
+                    testQuestionId = table.Column<long>(type: "bigint", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    manualScore = table.Column<int>(type: "int", nullable: false),
+                    autoScore = table.Column<int>(type: "int", nullable: false),
+                    windowViolation = table.Column<int>(type: "int", nullable: false),
+                    timeViolation = table.Column<int>(type: "int", nullable: false),
+                    comment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestAttemptQuestionSections", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestAttemptSections",
+                columns: table => new
+                {
+                    testAttemptSectionId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    testAttemptId = table.Column<long>(type: "bigint", nullable: false),
+                    testSectionId = table.Column<long>(type: "bigint", nullable: false),
+                    comment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestAttemptSections", x => x.testAttemptSectionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    testId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.testId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestSectionQuestions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    questionId = table.Column<long>(type: "bigint", nullable: false),
+                    testSectionId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestSectionQuestions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestSections",
+                columns: table => new
+                {
+                    testSectionId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    testId = table.Column<long>(type: "bigint", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestSections", x => x.testSectionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestTags",
+                columns: table => new
+                {
+                    testTagId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    testId = table.Column<long>(type: "bigint", nullable: false),
+                    tagId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestTags", x => x.testTagId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "QuestionTypes");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "TestAttemptQuestionSections");
+
+            migrationBuilder.DropTable(
+                name: "TestAttemptSections");
+
+            migrationBuilder.DropTable(
+                name: "Tests");
+
+            migrationBuilder.DropTable(
+                name: "TestSectionQuestions");
+
+            migrationBuilder.DropTable(
+                name: "TestSections");
+
+            migrationBuilder.DropTable(
+                name: "TestTags");
+
+            migrationBuilder.DropColumn(
+                name: "comment",
+                table: "TestAttempts");
+
+            migrationBuilder.DropColumn(
+                name: "createdOn",
+                table: "TestAttempts");
+
+            migrationBuilder.DropColumn(
+                name: "endDate",
+                table: "TestAttempts");
+
+            migrationBuilder.DropColumn(
+                name: "startDate",
+                table: "TestAttempts");
+
+            migrationBuilder.DropColumn(
+                name: "totalScore",
+                table: "TestAttempts");
+        }
+    }
+}
---
+++ Migrations/20230718202435_model-navigation-attempt-four.cs
@@ -0,0 +1,409 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IPR_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelnavigationattemptfour : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAttempts_Candidates_candidateId",
+                table: "TestAttempts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TestTags",
+                table: "TestTags");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TestSectionQuestions",
+                table: "TestSectionQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "testQuestionId",
+                table: "TestAttemptQuestionSections");
+
+            migrationBuilder.RenameTable(
+                name: "TestTags",
+                newName: "TestTag");
+
+            migrationBuilder.RenameTable(
+                name: "TestSectionQuestions",
+                newName: "TestSectionQuestion");
+
+            migrationBuilder.RenameColumn(
+                name: "testAttemptId",
+                table: "TestAttemptSections",
+                newName: "testAttemptattemptId");
+
+            migrationBuilder.RenameColumn(
+                name: "candidateId",
+                table: "TestAttempts",
+                newName: "candidateid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TestAttempts_candidateId",
+                table: "TestAttempts",
+                newName: "IX_TestAttempts_candidateid");
+
+            migrationBuilder.AddColumn<int>(
+                name: "candidateId",
+                table: "TestAttempts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "testSectionQuestionid",
+                table: "TestAttemptQuestionSections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TestTag",
+                table: "TestTag",
+                column: "testTagId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TestSectionQuestion",
+                table: "TestSectionQuestion",
+                column: "id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestSections_testId",
+                table: "TestSections",
+                column: "testId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestAttemptSections_testAttemptattemptId",
+                table: "TestAttemptSections",
+                column: "testAttemptattemptId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestAttemptSections_testSectionId",
+                table: "TestAttemptSections",
+                column: "testSectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestAttempts_candidateId",
+                table: "TestAttempts",
+                column: "candidateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestAttempts_testId",
+                table: "TestAttempts",
+                column: "testId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestAttemptQuestionSections_testAttemptSectionId",
+                table: "TestAttemptQuestionSections",
+                column: "testAttemptSectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestAttemptQuestionSections_testSectionQuestionid",
+                table: "TestAttemptQuestionSections",
+                column: "testSectionQuestionid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_questionTypeId",
+                table: "Questions",
+                column: "questionTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestTag_tagId",
+                table: "TestTag",
+                column: "tagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestTag_testId",
+                table: "TestTag",
+                column: "testId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestSectionQuestion_questionId",
+                table: "TestSectionQuestion",
+                column: "questionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestSectionQuestion_testSectionId",
+                table: "TestSectionQuestion",
+                column: "testSectionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_QuestionTypes_questionTypeId",
+                table: "Questions",
+                column: "questionTypeId",
+                principalTable: "QuestionTypes",
+                principalColumn: "questionTypeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAttemptQuestionSections_TestAttemptSections_testAttemptSectionId",
+                table: "TestAttemptQuestionSections",
+                column: "testAttemptSectionId",
+                principalTable: "TestAttemptSections",
+                principalColumn: "testAttemptSectionId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAttemptQuestionSections_TestSectionQuestion_testSectionQuestionid",
+                table: "TestAttemptQuestionSections",
+                column: "testSectionQuestionid",
+                principalTable: "TestSectionQuestion",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAttempts_Candidates_candidateId",
+                table: "TestAttempts",
+                column: "candidateId",
+                principalTable: "Candidates",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAttempts_Candidates_candidateid",
+                table: "TestAttempts",
+                column: "candidateid",
+                principalTable: "Candidates",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAttempts_Tests_testId",
+                table: "TestAttempts",
+                column: "testId",
+                principalTable: "Tests",
+                principalColumn: "testId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAttemptSections_TestAttempts_testAttemptattemptId",
+                table: "TestAttemptSections",
+                column: "testAttemptattemptId",
+                principalTable: "TestAttempts",
+                principalColumn: "attemptId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAttemptSections_TestSections_testSectionId",
+                table: "TestAttemptSections",
+                column: "testSectionId",
+                principalTable: "TestSections",
+                principalColumn: "testSectionId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestSectionQuestion_Questions_questionId",
+                table: "TestSectionQuestion",
+                column: "questionId",
+                principalTable: "Questions",
+                principalColumn: "questionId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestSectionQuestion_TestSections_testSectionId",
+                table: "TestSectionQuestion",
+                column: "testSectionId",
+                principalTable: "TestSections",
+                principalColumn: "testSectionId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestSections_Tests_testId",
+                table: "TestSections",
+                column: "testId",
+                principalTable: "Tests",
+                principalColumn: "testId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestTag_Tags_tagId",
+                table: "TestTag",
+                column: "tagId",
+                principalTable: "Tags",
+                principalColumn: "tagId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestTag_Tests_testId",
+                table: "TestTag",
+                column: "testId",
+                principalTable: "Tests",
+                principalColumn: "testId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_QuestionTypes_questionTypeId",
+                table: "Questions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAttemptQuestionSections_TestAttemptSections_testAttemptSectionId",
+                table: "TestAttemptQuestionSections");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAttemptQuestionSections_TestSectionQuestion_testSectionQuestionid",
+                table: "TestAttemptQuestionSections");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAttempts_Candidates_candidateId",
+                table: "TestAttempts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAttempts_Candidates_candidateid",
+                table: "TestAttempts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAttempts_Tests_testId",
+                table: "TestAttempts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAttemptSections_TestAttempts_testAttemptattemptId",
+                table: "TestAttemptSections");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestAttemptSections_TestSections_testSectionId",
+                table: "TestAttemptSections");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestSectionQuestion_Questions_questionId",
+                table: "TestSectionQuestion");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestSectionQuestion_TestSections_testSectionId",
+                table: "TestSectionQuestion");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestSections_Tests_testId",
+                table: "TestSections");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestTag_Tags_tagId",
+                table: "TestTag");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestTag_Tests_testId",
+                table: "TestTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestSections_testId",
+                table: "TestSections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestAttemptSections_testAttemptattemptId",
+                table: "TestAttemptSections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestAttemptSections_testSectionId",
+                table: "TestAttemptSections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestAttempts_candidateId",
+                table: "TestAttempts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestAttempts_testId",
+                table: "TestAttempts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestAttemptQuestionSections_testAttemptSectionId",
+                table: "TestAttemptQuestionSections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestAttemptQuestionSections_testSectionQuestionid",
+                table: "TestAttemptQuestionSections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_questionTypeId",
+                table: "Questions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TestTag",
+                table: "TestTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestTag_tagId",
+                table: "TestTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestTag_testId",
+                table: "TestTag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TestSectionQuestion",
+                table: "TestSectionQuestion");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestSectionQuestion_questionId",
+                table: "TestSectionQuestion");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestSectionQuestion_testSectionId",
+                table: "TestSectionQuestion");
+
+            migrationBuilder.DropColumn(
+                name: "candidateId",
+                table: "TestAttempts");
+
+            migrationBuilder.DropColumn(
+                name: "testSectionQuestionid",
+                table: "TestAttemptQuestionSections");
+
+            migrationBuilder.RenameTable(
+                name: "TestTag",
+                newName: "TestTags");
+
+            migrationBuilder.RenameTable(
+                name: "TestSectionQuestion",
+                newName: "TestSectionQuestions");
+
+            migrationBuilder.RenameColumn(
+                name: "testAttemptattemptId",
+                table: "TestAttemptSections",
+                newName: "testAttemptId");
+
+            migrationBuilder.RenameColumn(
+                name: "candidateid",
+                table: "TestAttempts",
+                newName: "candidateId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TestAttempts_candidateid",
+                table: "TestAttempts",
+                newName: "IX_TestAttempts_candidateId");
+
+            migrationBuilder.AddColumn<long>(
+                name: "testQuestionId",
+                table: "TestAttemptQuestionSections",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TestTags",
+                table: "TestTags",
+                column: "testTagId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TestSectionQuestions",
+                table: "TestSectionQuestions",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestAttempts_Candidates_candidateId",
+                table: "TestAttempts",
+                column: "candidateId",
+                principalTable: "Candidates",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
