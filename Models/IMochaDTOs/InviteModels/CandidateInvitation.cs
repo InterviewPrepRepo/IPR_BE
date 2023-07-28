a4321c80bba4649ab1d1fd5622683fd404a5f160
Minseon Song <songminseonk@gmail.com>
2023-07-28 12:15:42 -0400

    added comment for grade end point

--- Models/IMochaDTOs/InviteModels/CandidateInvitation.cs
+++ Models/IMochaDTOs/InviteModels/CandidateInvitation.cs
@@ -2,7 +2,6 @@ namespace IPR_BE.Models;
 
 /// <summary>
 /// This is a DTO we ask OUR clients to submit to us to invite candidates
-/// By default, it asks iMocha to send email behalf of us
 /// </summary>
 public class CandidateInvitation {
     public int testId { get; set; }
